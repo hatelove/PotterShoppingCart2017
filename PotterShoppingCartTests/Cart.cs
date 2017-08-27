bf7501a37467865347c1b9673314d7185a8543f0
hatelove <jay.hatelove@gmail.com>
2017-08-27 14:29:57 +0800

    pass test by add dictionary key and calculate 3 and 5 pair by 4 and 4 pair

--- PotterShoppingCartTests/Cart.cs
+++ PotterShoppingCartTests/Cart.cs
@@ -24,18 +24,44 @@ namespace PotterShoppingCartTests
         public decimal Checkout(IEnumerable<Book> books)
         {
             var suites = GetSuites(books);
-            return suites.Sum(s => AmountOfEachSuite(s));
+
+            var fiveBooksOfSuite = suites.Where(x => x.Value.Count() == 5).ToList();
+
+            var threeBooksOfSuite = suites.Where(x => x.Value.Count() == 3).ToList();
+
+
+            var threeAndFivePairs = fiveBooksOfSuite.Zip(threeBooksOfSuite, (five, three) =>
+            {
+                return new { five, three };
+            });
+
+            decimal chooseCheaperDiscountTotal = 0;
+            foreach (var threeAndFivePair in threeAndFivePairs)
+            {
+                chooseCheaperDiscountTotal += 640;
+                suites.Remove(threeAndFivePair.five.Key);
+                suites.Remove(threeAndFivePair.three.Key);
+            }
+
+            return chooseCheaperDiscountTotal + suites.Sum(s => AmountOfEachSuite(s.Value));
         }
 
-        private IEnumerable<IEnumerable<Book>> GetSuites(IEnumerable<Book> books)
+        private Dictionary<int, IEnumerable<Book>> GetSuites(IEnumerable<Book> books)
         {
+            var index = 0;
+            var result = new Dictionary<int, IEnumerable<Book>>();
+
             var unCheckedoutBooks = books;
+
             while (unCheckedoutBooks.Any())
             {
                 var suite = unCheckedoutBooks.GroupBy(b => b.ISBN).Select(x => x.First());
-                yield return suite;
+                result.Add(index, suite);
                 unCheckedoutBooks = unCheckedoutBooks.Except(suite);
+                index++;
             }
+
+            return result;
         }
 
         private decimal AmountOfEachSuite(IEnumerable<Book> suiteOfBooks)
@@ -50,6 +76,19 @@ namespace PotterShoppingCartTests
         {
             var count = books.Count();
             return this._discount[count];
+        }
+    }
+
+    internal class BooksComparer : IEqualityComparer<IEnumerable<Book>>
+    {
+        public bool Equals(IEnumerable<Book> x, IEnumerable<Book> y)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetHashCode(IEnumerable<Book> obj)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
